6657a38be3b27e9fb5e45517a49d0b39cb2cdab5
vnuxa <123472826+vnuxa@users.noreply.github.com>
2023-02-07 05:56:50 +0000

    asd

--- Modules/Libraries/userLib.lua
+++ Modules/Libraries/userLib.lua
@@ -6,7 +6,19 @@ function module:Init(discordia,client)
     function lib:GetUserFromStringID(guild,arg)
         return guild:getMember(arg)
     end
-    
+    --[[
+        Discord perms:
+        "Public",
+    ]]
+    function lib:CheckDiscordPermission(member,perm)
+        local req = string.lower(perm)
+        if req == "public" then return true end 
+        if req == "hc" then return member:hasPermission("banMembers") end 
+        if req == "mc" then return member:hasPermission("moveMembers") end
+        print("No available permissions, got:",req)
+        return nil
+    end
+
 
     
     return lib
